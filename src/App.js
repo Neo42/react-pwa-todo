3c20ad90e90248444883a82ebc1b7f18087079ac
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-03-24 20:38:29 +0800

    feat: ✨ add routing and profile page

--- src/App.js
+++ src/App.js
@@ -1,40 +1,71 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom'
+
 import logo from './logo.svg'
 import './App.css'
 
+import GreyProfile from './grey_profile.png'
+
 const ITEMS_URL = 'http://192.168.199.102:4567/items.json'
 
-function App() {
-  const [items, setItems] = React.useState([])
-  const [isLoading, setIsLoading] = React.useState(true)
-  const [todoItem, setTodoItem] = React.useState('')
-  // navigator.onLine indicates the presence of a network connection, not a server connection
-  const [isOffline, setIsOffline] = React.useState(!navigator.onLine)
+const Profile = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          Profile
+        </span>
+      </nav>
 
-  React.useEffect(() => {
+      <div style={{textAlign: 'center'}}>
+        <img
+          src={GreyProfile}
+          alt="profile"
+          style={{height: 200, marginTop: 50}}
+        />
+        <p style={{color: '#888', fontSize: 20}}>username</p>
+      </div>
+    </div>
+  )
+}
+
+class List extends Component {
+  state = {
+    items: [],
+    loading: true,
+    todoItem: '',
+    offline: !navigator.onLine,
+  }
+
+  componentDidMount() {
     fetch(ITEMS_URL)
       .then((response) => response.json())
       .then((items) => {
-        setItems(items)
-        setIsLoading(false)
+        this.setState({items, loading: false})
       })
 
-    window.addEventListener('online', updateIsOffline)
-    window.addEventListener('offline', updateIsOffline)
-    return () => {
-      window.removeEventListener('online', updateIsOffline)
-      window.removeEventListener('offline', updateIsOffline)
-    }
-  }, [])
+    window.addEventListener('online', this.setOfflineStatus)
+    window.addEventListener('offline', this.setOfflineStatus)
+  }
 
-  const updateIsOffline = () => setIsOffline(!navigator.onLine)
+  componentWillUnmount() {
+    window.removeEventListener('online', this.setOfflineStatus)
+    window.removeEventListener('offline', this.setOfflineStatus)
+  }
 
-  const addItem = (e) => {
+  setOfflineStatus = () => {
+    this.setState({offline: !navigator.onLine})
+  }
+
+  addItem = (e) => {
     e.preventDefault()
 
     fetch(ITEMS_URL, {
       method: 'POST',
-      body: JSON.stringify({item: todoItem}),
+      body: JSON.stringify({item: this.state.todoItem}),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -44,14 +75,14 @@ function App() {
         if (items.error) {
           alert(items.error)
         } else {
-          setItems(items)
+          this.setState({items})
         }
       })
 
-    setTodoItem('')
+    this.setState({todoItem: ''})
   }
 
-  const deleteItem = (itemId) => {
+  deleteItem = (itemId) => {
     fetch(ITEMS_URL, {
       method: 'DELETE',
       body: JSON.stringify({id: itemId}),
@@ -64,68 +95,93 @@ function App() {
         if (items.error) {
           alert(items.error)
         } else {
-          setItems(items)
+          this.setState({items})
         }
       })
   }
-  return (
-    <div className="App">
-      <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1">
-          <img src={logo} className="App-logo col-3" alt="logo" />
-          PWA Todo List
-        </span>
-        {isOffline ? (
-          <span className="badge badge-danger my-3">Offline</span>
-        ) : null}
-      </nav>
 
-      <div className="px-3 py-2">
-        <form className="form-inline my-3" onSubmit={addItem}>
-          <div className="form-group mb-2 p-0 pr-3 col-8 col-sm-10">
-            <input
-              className="form-control col-12"
-              placeholder="What do you need to do?"
-              value={todoItem}
-              onChange={(e) => setTodoItem(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary mb-2 col-4 col-sm-2">
-            Add
-          </button>
-        </form>
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar navbar-light bg-light">
+          <span className="navbar-brand mb-0 h1">
+            <img src={logo} alt="logo" />
+            Todo List
+          </span>
+          {this.state.offline && (
+            <span className="badge badge-danger my-3">Offline</span>
+          )}
+          <span>
+            <Link to="/profile">Profile</Link>
+          </span>
+        </nav>
 
-        {isLoading ? <p>Loading...</p> : null}
+        <div className="px-3 py-2">
+          <form className="form-inline my-3" onSubmit={this.addItem}>
+            <div className="form-group mb-2 p-0 pr-3 col-8 col-sm-10">
+              <input
+                className="form-control col-12"
+                placeholder="What do you need to do?"
+                value={this.state.todoItem}
+                onChange={(e) =>
+                  this.setState({
+                    todoItem: e.target.value,
+                  })
+                }
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary mb-2 col-4 col-sm-2">
+              Add
+            </button>
+          </form>
 
-        {!isLoading && items.length === 0 && (
-          <div className="alert alert-secondary">No items - all done!</div>
-        )}
+          {this.state.loading && <p>Loading...</p>}
 
-        {!isLoading && items && (
-          <table className="table table-striped">
-            <tbody>
-              {items.map((item, i) => {
-                return (
-                  <tr key={item.id} className="row">
-                    <td className="col-1">{i + 1}</td>
-                    <td className="col-10">{item.item}</td>
-                    <td className="col-1">
-                      <button
-                        type="button"
-                        className="close"
-                        aria-label="Close"
-                        onClick={() => deleteItem(item.id)}>
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-        )}
+          {!this.state.loading && this.state.items.length === 0 && (
+            <div className="alert alert-secondary">No items - all done!</div>
+          )}
+
+          {!this.state.loading && this.state.items && (
+            <table className="table table-striped">
+              <tbody>
+                {this.state.items.map((item, i) => {
+                  return (
+                    <tr key={item.id} className="row">
+                      <td className="col-1">{i + 1}</td>
+                      <td className="col-10">{item.item}</td>
+                      <td className="col-1">
+                        <button
+                          type="button"
+                          className="close"
+                          aria-label="Close"
+                          onClick={() => this.deleteItem(item.id)}>
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          )}
+        </div>
       </div>
-    </div>
+    )
+  }
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<List />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
